145a9ba04a0df063110170f306021e2787def6f3
Pablo Franco <pabloafranco@hotmail.com>
2021-09-04 16:46:38 -0300

    uno a uno

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
 import peewee
 
-
 database = peewee.MySQLDatabase('pythondb', 
                                  host='localhost', 
                                  port=3306, 
                                  user='root', 
                                  passwd='root')
 
-class Author(peewee.Model):
-    name = peewee.CharField(max_length=50)
+class User(peewee.Model):
+    username = peewee.CharField(max_length=50)
+    email = peewee.CharField(max_length=50)
 
     class Meta:
-        database = database
-        db_table = 'authors'
+        database=database
+        db_table = 'users'
 
     def __str__(self):
-        return self.name
+        return self.username
 
-class Book(peewee.Model):
-    title = peewee.CharField(max_length=50)
-    author = peewee.ForeignKeyField(Author)
+    #Asi trato al metodo como un atributo!!
+    @property
+    def admin(self):
+        return self.admins.first()
+
+class Admin(peewee.Model):
+    permission_level = peewee.IntegerField(default=1)
+    user = peewee.ForeignKeyField(User, backref='admins', unique=True)
 
     class Meta:
-        database = database
-        db_table = 'books'
+        database=database
+        db_table = 'admins'
 
     def __str__(self):
-        return self.title
+        return 'Admin '  + str(self.id)
 
+    
 
 if __name__ == '__main__':
-    database.drop_tables([Author, Book])
-    database.create_tables([Author, Book])
+    database.drop_tables([User, Admin])
+    database.create_tables([User, Admin])
 
-    author1 = Author.create(name='Stephen Key')
+    user1 = User.create(username='User1', email='example1@email.com')
+    admin1 = Admin.create(permission_level=10, user=user1)
 
-    book1 = Book.create(title='It', author=author1)
-    book1 = Book.create(title='El resplandor', author=author1)
-    book1 = Book.create(title='Cujo', author=author1)
+    print (user1.username)
+    print (admin1.user)
+    print (admin1.user.email)
+
+    print (user1.admins.first())
+    print (user1.admin)
